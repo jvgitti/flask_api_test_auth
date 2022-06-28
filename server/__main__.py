69c123e4ee8e848a8892e6021a6cb9898929baae
jvgitti <jvgitti@gmail.com>
2022-06-27 21:20:16 -0300

    Adicionando Dockerfile

--- server/__main__.py
+++ server/__main__.py
@@ -3,8 +3,9 @@ from server import init_api
 
 def main():
     app = init_api()
-    app.run(host='localhost', port='8080')
+    app.run(host='0.0.0.0')
 
 
 if __name__ == "__main__":
     main()
+
